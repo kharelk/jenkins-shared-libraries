154467b0b6a817e5c000dc981812be01c05795d0
Harel Karavani <harelkaravani@Harels-MacBook-Pro.local>
2022-06-27 07:44:30 +0300

    chenged file in vars

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,5 @@
 def call(Map config = [:]) {
-    sh "echo Hello ${config.name}, Today is ${config.dayOfWeek}"
+    sh "echo Hello ${config.name}, Today is ${config.dayOfWeek}'n"
+    sh "echo Hello ${config.test}'n"
 }
 
